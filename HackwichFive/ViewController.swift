922f52aa1feeac56196a887bf587f55e31888220
Makayla <mcsyanos@hawaii.edu>
2018-02-27 09:24:07 -1000

    Completed Hackwich #5 with myFriendsArray list

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    //Declare array here
+    var myFriendsArray = ["Mike Wazowski", "Woody", "Belle", "Winnie the Pooh", "Stitch"]
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
+        self.tableView.dataSource = self
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -20,6 +24,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+      
+        return 1;
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return myFriendsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellResuseIdentifier")!
+        
+        let text = myFriendsArray[indexPath.row]
+        
+        cell.textLabel?.text = text
+        
+        return cell
+    }
 
 }
 
